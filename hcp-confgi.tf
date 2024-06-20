c7b97273e179885a0efa97ad59c3ff51d694990b
Chris MacMurray <devopscmac@gmail.com>
2024-06-20 14:44:41 -0400

    Added a proper VPC

--- hcp-confgi.tf
+++ hcp-confgi.tf
@@ -1,6 +1,7 @@
 terraform {
   cloud {
     organization = "devopscmac"
+    required_version = "1.8.5"
 
     workspaces {
       name = "aiml-playground-core-networking"
--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,26 @@
-# Create a VPC
-resource "aws_vpc" "aiml-vpc" {
-  cidr_block = "192.168.0.0/24"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "AIML-VPC"
+  cidr = "10.0.0.0/16"
+
+  azs                    = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets         = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  elastic_search_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+  sagemanker_subnets     = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "AIML"
+    name        = "AIML-VPC"
+  }
 }
